ecee1e9c37ea3c611b8ddcc0f35ae6b83259e859
ErikKoop <erikkoop@web.de>
2023-04-05 11:54:19 +0200

    Aufgabe Fertig

--- shop.php
+++ shop.php
@@ -1,8 +1,8 @@
 <?php include('./template/navbar.php'); ?>
 
 <div class="container pt-3">
-    <h3>Shop</h3>
-    <img src="http://localhost/ek/bilder/shop1.jpg"height="400" width="700">
+    <h1>Shop</h1>
+   
 </div>
 
 
--- test.php
+++ test.php
@@ -25,7 +25,7 @@ $answerSelectQurey = $qp->selectQuery($selectQuery);
 ?>
 
 <div class="container pt-3">
-    <h3>DAA Anwendungsentwicklerin Nadine Stamm</h3>
+    <h1>DAA AE Erik Koop</h1>
     <?=var_dump($answerSelectQurey)?>
 </div>
 
